96d74e188e11db5d9e2cb0896e972b8c93aa270d
JesraAli <jesra.ali@gmail.com>
2023-06-10 23:01:07 +0100

    Add comments and clean up code

--- Problem003.hs
+++ Problem003.hs
@@ -1,6 +1,3 @@
-
-
-
 --Create Square Root Function
 sqRoot :: Int->Int
 sqRoot x =  round (sqrt(fromIntegral x))
@@ -10,4 +7,3 @@ sqRoot x =  round (sqrt(fromIntegral x))
 --Checks if prime number, returns last value in list
 largestPF :: Int->Int
 largestPF n = last([x | x <- [1..sqRoot n], x*x < n, n `mod` x == 0, [m | m <- [1..x], x `mod` m ==0] == [1,x]]) 
-
