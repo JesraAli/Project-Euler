6a8a86c9d03d5bb9fc937fe83d347c5c25255099
JesraAli <jesra.ali@gmail.com>
2023-06-21 00:06:10 +0100

    Optimise solution to one function

--- Problem006.hs
+++ Problem006.hs
@@ -1,8 +1,5 @@
-sumOfSquares :: Int -> Int
-sumOfSquares x =  sum (map(^2) [1..x])
-
-squareOfSum :: Int -> Int
-squareOfSum x = sum [1..x] ^2
-
-difference :: Int->Int
-difference x = squareOfSum x - sumOfSquares x
+difference ::Int->Int
+difference x = squareOfSum - sumOfSquares
+                where 
+                    sumOfSquares = sum (map(^2) [1..x])
+                    squareOfSum = sum [1..x] ^2
