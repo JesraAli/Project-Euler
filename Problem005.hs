7f57c5b62636fff97114f2df643ebfeb8ebdf9f6
JesraAli <jesra.ali@gmail.com>
2023-09-04 16:52:02 +0100

    Change recursive function to using foldr()

--- Problem005.hs
+++ Problem005.hs
@@ -1,5 +1,3 @@
-
-
 --2520  is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 --What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  
@@ -8,11 +6,8 @@ smallestNum :: Int
 smallestNum = findSmallestNum 1
     where
         findSmallestNum n 
-            | isDivisible n [2..20]  /= False = n
+            | isDivisible n [2..20]  == True = n
             | otherwise = findSmallestNum (n+1)
 
-
 isDivisible:: Int->[Int]->Bool
-isDivisible n [] = True
-isDivisible n (m:xs) | n `mod` m == 0 = isDivisible n xs
-                    | otherwise = False
+isDivisible n = foldr (\m -> (&&) (n `mod` m == 0)) True
