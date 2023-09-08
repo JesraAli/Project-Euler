0ccff970a7a9f5f3c5a96e11f1d383a6ae779757
JesraAli <jesra.ali@gmail.com>
2023-09-08 17:55:40 +0100

    Add comments

--- Problem016.hs
+++ Problem016.hs
@@ -1,12 +1,13 @@
-import Data.Char (digitToInt, intToDigit)
-
+import Data.Char (digitToInt)
 
 callNum ::Int
 callNum = sumOfDigits (2^1000) 0
 
+--Used Integer, so it can read 2^1000
 sumOfDigits :: Integer->Int-> Int
 sumOfDigits 0 finalSum = finalSum
-sumOfDigits n finalSum = finalSum + digitToInt(head (show n) ) + sumOfDigits (foldl (\mergedInt digit -> mergedInt * 10 + toInteger digit) 0 (map digitToInt(tail (show n))))  finalSum
 
-test ::Integer->Integer
-test n = (foldl (\mergedInt digit -> mergedInt * 10 + toInteger digit) 0 (map digitToInt(tail (show n)))) --merge the number together using foldL (0 is starting value of 'mergedInt)
+--Added the first digit of the number to 'finalSum', then recursively called sumOfDigits until n was 0
+--Retrieved the tail of the number, and using 'map', converted each digit to an Int
+--Then used foldl to merge the returned list values together
+sumOfDigits n finalSum = finalSum + digitToInt(head (show n) ) + sumOfDigits (foldl (\mergedInt digit -> mergedInt * 10 + toInteger digit) 0 (map digitToInt(tail (show n))))  finalSum
